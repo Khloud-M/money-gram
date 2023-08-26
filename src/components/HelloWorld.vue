a58e82a077967ddb546078cc6913ed5656e9f7f8
Khloud <62654747+Khloud-M@users.noreply.github.com>
2023-08-26 03:25:40 +0300

    upddate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,24 +10,26 @@
       </div>
       <!-- end process -->
       <div class="steps">
+        <hr/>
         <h5>الخطوات التالية :</h5>
-        <p>على المستلم إحضار بطاقة الهويةورقما مرجعيا لاستلام المال</p>
-        <span> ابحث عن الموقع </span>
-        
+        <p>على المستلم إحضار بطاقة الهوية و رقم مرجعيا لاستلام المال</p>
+        <span> ابحث عن موقع </span>
+
         <div class="details_proccess">
           <h5>تفاصيل العملية :</h5>
           <table class="table">
-          <tbody>
-            <tr v-for="(item, index) in tableData" :key="index">
-              <td v-html="item.column1"></td>
-              <td>{{ item.column2 }}</td>
-              <td>{{ item.column3 }}</td>
-            </tr>
-          </tbody>
-        </table>
+            <tbody>
+              <tr v-for="(item, index) in tableData" :key="index">
+                <td v-html="item.column1"></td>
+                <td>{{ item.column2 }}</td>
+                <td>{{ item.column3 }}</td>
+              </tr>
+            </tbody>
+          </table>
+          <!-- end table -->
           <div class="notes">
             <p>
-              ملاحظة الإيصالات عبر الإنترنت غير متوفرةلمعاملات التجارية في
+              ملاحظة الإيصالات عبر الإنترنت غير متوفرة لمعاملات التجارية في
               الموقع
             </p>
           </div>
@@ -51,7 +53,7 @@ export default {
         { column1: "", column2: "", column3: "جاهز للاستلام" },
         {
           column1:
-            '<svg fill="#000000" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve" width="30px" height="30px"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path id="time_1_" d="M16,31.36C7.53,31.36,0.64,24.47,0.64,16S7.53,0.64,16,0.64S31.36,7.53,31.36,16S24.47,31.36,16,31.36z M15.64,28h0.72v2.636c7.788-0.188,14.087-6.488,14.276-14.276H28v-0.72h2.636C30.447,7.853,24.147,1.553,16.36,1.364V4h-0.72V1.364C7.853,1.553,1.553,7.853,1.364,15.64H4v0.72H1.364c0.189,7.788,6.488,14.087,14.276,14.276V28z M22.748,20.312l-7.108-4.104V6h0.72v9.792l6.748,3.896L22.748,20.312z"></path><rect id="_Transparent_Rectangle" style="fill: none" width="15" height="15"></rect></g></svg>',
+            '<svg fill="#000000" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve" width="25px" height="25px"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path id="time_1_" d="M16,31.36C7.53,31.36,0.64,24.47,0.64,16S7.53,0.64,16,0.64S31.36,7.53,31.36,16S24.47,31.36,16,31.36z M15.64,28h0.72v2.636c7.788-0.188,14.087-6.488,14.276-14.276H28v-0.72h2.636C30.447,7.853,24.147,1.553,16.36,1.364V4h-0.72V1.364C7.853,1.553,1.553,7.853,1.364,15.64H4v0.72H1.364c0.189,7.788,6.488,14.087,14.276,14.276V28z M22.748,20.312l-7.108-4.104V6h0.72v9.792l6.748,3.896L22.748,20.312z"></path><rect id="_Transparent_Rectangle" style="fill: none" width="15" height="15"></rect></g></svg>',
           column2: "123456789",
           column3: "الرقم المرجعي",
         },
@@ -77,14 +79,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" >
-.table {
-  border-collapse: collapse;
-}
-
-.table th,
-.table td {
-  padding: 8px;
-}
 .process {
   display: flex;
   flex-direction: row;
@@ -97,23 +91,44 @@ export default {
   // }
   div {
     background-color: gray;
-    // border-radius: 20px 0 0 30px;
   }
 }
-.details_proccess {
+.steps {
   background-color: #f6f6f6;
   padding: 30px;
-  div {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    border-bottom: 1px solid #c1c4c3;
-    padding: 3px;
-    align-items: center;
+  margin-top: 50px;
+  hr{
+    background-color: #969a98;
+    margin-top: 50px;
+  }
+  p{
+    font-weight: 700;
+    
+  }
+  span {
+    color: #4998af;
+    font-weight: 700;
+  }
+  .details_proccess {
+    margin-top: 20px;
+  }
+  .notes {
+    color: #565f5c;
     color: #969a98;
+    margin-top: 30px;
+    font-size: 20px;
   }
 }
-.notes {
-  color: #565f5c;
+.table {
+  border-collapse: collapse;
+}
+.table th,
+.table td {
+  padding: 8px;
+  color: #969a98;
+  font-size: 18px;
+}
+.table tr:last-child td {
+  border-bottom: none;
 }
 </style>
